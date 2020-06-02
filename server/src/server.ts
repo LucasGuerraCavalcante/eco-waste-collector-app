5f01d23137215c02160d6d70469caedcc1b4eb33
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-06-02 16:14:16 -0300

    database connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,43 +1,9 @@
-import express, { response } from 'express'
+import express from 'express'
+import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
-
-const users = [
-    'Paulo',
-    'Max',
-    'Igor',
-    'Adreas',
-    'Jairo',
-    'Wagner'
-]
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search)
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users
-
-    response.json(filteredUsers)
-})
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id)
-
-    const user = users[id]
-
-    return response.json(user)
-})
-
-app.post('/users', (request, response) => {
-    const data = request.body
-
-    const user = {
-        name: data.name,
-        email: data.email
-    }
-
-    return response.json(user)
-})
+app.use(routes)
 
 app.listen(3333)
