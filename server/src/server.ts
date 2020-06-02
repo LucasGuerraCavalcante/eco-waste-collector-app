1473a13793afa90d04e321faf0b5bf2ef7bbeab4
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-06-02 19:36:55 -0300

    icons

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import routes from './routes'
 
 const app = express()
@@ -6,4 +7,6 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+// <div>Ícones feitos por <a href="https://www.flaticon.com/br/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/br/" title="Flaticon">www.flaticon.com</a></div>
 app.listen(3333)
