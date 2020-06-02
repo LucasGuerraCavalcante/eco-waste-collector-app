64fb1defd60cd1f76c49e161f96f9a6e7debeba6
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-06-02 15:57:30 -0300

    get and post tests

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,39 @@
-import express from 'express'
+import express, { response } from 'express'
 
 const app = express()
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de Usuarios')
+const users = [
+    'Paulo',
+    'Max',
+    'Igor',
+    'Adreas',
+    'Jairo',
+    'Wagner'
+]
 
-    response.json([
-        'Paulo',
-        'Max',
-        'Igor',
-        'Adreas',
-        'Jairo'
-    ])
+app.get('/users', (request, response) => {
+    const search = String(request.query.search)
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users
+
+    response.json(filteredUsers)
+})
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id)
+
+    const user = users[id]
+
+    return response.json(user)
+})
+
+app.post('/users', (request, response) => {
+    const user = {
+        name: 'Lucas',
+        email: 'lucas@email.com'
+    }
+
+    return response.json(user)
 })
 
 app.listen(3333)
