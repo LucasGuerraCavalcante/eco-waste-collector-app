dc1baf46defdf411ad3797aac26fa07d501755c2
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-06-02 18:46:57 -0300

    executing the new migrations

--- server/src/database/migrations/02_create_pont_items.ts
+++ server/src/database/migrations/02_create_pont_items.ts
@@ -1,7 +1,21 @@
-export async function up() {
+import Knex from 'knex'
 
+export async function up(knex: Knex) {
+return knex.schema.createTable('point_items', table => {
+    table.increments('id').primary()
+
+    table.integer('point_id')
+        .notNullable()
+        .references('id')
+        .inTable('point')
+
+    table.integer('item_id')
+        .notNullable()
+        .references('id')
+        .inTable('items')
+})
 }
-
-export async function down() {
-    
+ 
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('point_items')
 }
