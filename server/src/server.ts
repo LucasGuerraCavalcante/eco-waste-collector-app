1a953e49debb50fe41d7fdfe81c0b01cca8932a1
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-06-02 20:30:13 -0300

    point_items tables insert

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,4 +9,5 @@ app.use(routes)
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 // <div>Ícones feitos por <a href="https://www.flaticon.com/br/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/br/" title="Flaticon">www.flaticon.com</a></div>
+
 app.listen(3333)
