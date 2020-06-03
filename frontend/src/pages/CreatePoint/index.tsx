85bed730fe10d5865071c190fdcfa4c4d36dc866
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-06-03 19:28:29 -0300

    item list integration api

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
-
 import { Map, TileLayer, Marker } from 'react-leaflet'
+
+import api from '../../services/api'
 
 import './styles.css'
 import logo from '../../assets/logo.svg'
 
+interface Item {
+    id: number,
+    title: string,
+    image_url: string
+}
+
 const CreatePoint = () => {
+
+    const [items, setItems] = useState<Item[]>([])
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data)
+        })
+    }, [])
+
     return (
         <div id="page-create-point">
             <header>
@@ -96,30 +112,12 @@ const CreatePoint = () => {
                     </legend>
 
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/uploads/oil.svg" alt="Teste"></img>
-                            <span>Kitchen Oil</span>
-                        </li>    
-                        <li>
-                            <img src="http://localhost:3333/uploads/oil.svg" alt="Teste"></img>
-                            <span>Kitchen Oil</span>
-                        </li>  
-                        <li>
-                            <img src="http://localhost:3333/uploads/oil.svg" alt="Teste"></img>
-                            <span>Kitchen Oil</span>
-                        </li>  
-                        <li>
-                            <img src="http://localhost:3333/uploads/oil.svg" alt="Teste"></img>
-                            <span>Kitchen Oil</span>
-                        </li>  
-                        <li>
-                            <img src="http://localhost:3333/uploads/oil.svg" alt="Teste"></img>
-                            <span>Kitchen Oil</span>
-                        </li>  
-                        <li>
-                            <img src="http://localhost:3333/uploads/oil.svg" alt="Teste"></img>
-                            <span>Kitchen Oil</span>
-                        </li>   
+                        { items.map(item => (
+                            <li key={item.id}>
+                                <img className="icon" src={item.image_url} alt={item.title}></img>
+                             <span>{item.title}</span>
+                            </li>  
+                        ))}    
                     </ul>
                 </fieldset>
 
