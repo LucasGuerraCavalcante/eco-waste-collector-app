cedc3834020d896f7ff2d469615356f49bb3c01d
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-06-03 15:26:56 -0300

    Header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Header from './Header'
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header title="Ecological Waste Management" />
+      <h1>Conteudo do app</h1>
+    </div>
   );
 }
 
