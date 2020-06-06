d1229bc7b3d9cb3bc4980745a3b8be16c93061d9
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-06-05 22:50:51 -0300

    backend validation

--- api_server/src/server.ts
+++ api_server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import path from 'path'
 import routes from './routes'
+import { errors } from 'celebrate'
 
 const app = express()
 
@@ -11,5 +12,7 @@ app.use(routes)
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 // <div>Ícones feitos por <a href="https://www.flaticon.com/br/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/br/" title="Flaticon">www.flaticon.com</a></div>
+
+app.use(errors())
 
 app.listen(3333)
