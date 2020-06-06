a207464dd17b9d6a1dc373cc75c89e978aa7a2de
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-06-05 21:01:24 -0300

    image url config

--- ui_mobile/src/pages/Points/index.tsx
+++ ui_mobile/src/pages/Points/index.tsx
@@ -19,6 +19,7 @@ interface Points {
   id: number
   name: string
   image: string
+  image_url: string
   latitude: number
   longitude: number
 }
@@ -139,7 +140,7 @@ const Points = () => {
                                   <View style={styles.mapMarkerContainer}>
                                       <Image 
                                           style={styles.mapMarkerImage} 
-                                          source={{ uri: point.image }}>
+                                          source={{ uri: point.image_url }}>
                                       </Image>
                                       <Text style={styles.mapMarkerTitle}>{point.name}</Text>
                                   </View>
